989eb4ed1331de6acde1b8e7fd9f24cd91789806
thangtranit90 <thangtranit90@gmail.com>
2024-11-15 22:36:31 +0700

    first commit

---
+++ 5-basic-labs/02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -0,0 +1,21 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 5.31" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-06b21ccaeff8cd686" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
+}
